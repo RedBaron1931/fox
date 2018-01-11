d5808c51d8cd154ae104ad6df33d4c490bf8ee56
RedBaron1931 <redbaron1931@gmail.com>
2018-01-11 22:06:25 +0200

    1.1 OWL

--- app/js/script.js
+++ app/js/script.js
@@ -1,10 +1,5 @@
 $(document).ready(function () {
-    $(".right_services").owlCarousel({
-        margin: 10,
-        loop: true,
-        autoWidht: true,
-        items: 4
+    $(".owl-carousel .right_services").owlCarousel({
 
     });
-
 });
